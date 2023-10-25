bf4b936ddcfbcc7d9c44ebfec470384e87c7f80f
Bo Gir <mues1i@yandex.ru>
2023-10-26 00:01:16 +0700

    5 спринт (проверен)

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,10 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // Функция создания карточки
 function createCard(cardItem, deleteCard) {
-  const cardLi = cardTemplate.querySelector('.card').cloneNode(true);
-  const cardImage = cardLi.querySelector('.card__image');
-  const cardNameImage = cardLi.querySelector('.card__title');
-  const deleteButton = cardLi.querySelector('.card__delete-button');
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardNameImage = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
   cardNameImage.textContent = cardItem.name;
   cardImage.alt = cardItem.name;
@@ -15,7 +15,7 @@ function createCard(cardItem, deleteCard) {
   //Удаление карточки
   deleteButton.addEventListener('click', deleteCard);
   
-  return cardLi;
+  return cardElement;
 }
 
 //Функция удаления карточки
