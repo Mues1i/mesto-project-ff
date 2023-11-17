3155ad10ab311c9cdf8b4e256129af91e4eaf0b2
Bo Gir <mues1i@yandex.ru>
2023-11-17 15:17:48 +0700

    6 спринт Настройка WebPack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import '../pages/index.css'; // добавьте импорт главного файла стилей
+import initialCards from './cards';
+
 const cardsList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
